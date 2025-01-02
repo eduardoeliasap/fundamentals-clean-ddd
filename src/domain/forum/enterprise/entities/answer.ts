bce46f354ee53dacc9f09c0bb9d812e680f138ee
Eduardo Elias <eduardo.eliasap@gmail.com>
2025-01-02 14:01:30 -0300

    adding fetch recent question use-case

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -54,7 +54,7 @@ export class Answer extends Entity<AnswerProps> {
     const answer = new Answer(
       {
         ...props,
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
